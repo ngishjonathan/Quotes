b85d03c3b5f33eb7e3d20a6095affee8b37ab754
ngishjonathan <balozimwash@gmil.com>
2019-04-29 07:27:22 +0300

    added a showDescription property which is a boolean value

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -7,18 +7,21 @@ import {Quote} from '../quote'
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
-
   quotes = [
-    new Quote(1,'Never give up in this life'),
-    new Quote(2,'Remain calm it does not hurt'),
-    new Quote(3, 'Never chase a Dream'),
-    new Quote(4, 'when the deal is too good think twice'),
-    new Quote(5, 'character makes a man'),
-    new Quote(6, 'work hard today to avoid hardwork tommorrow'),
-    ]
+    new Quote(1,'Never give up in this life','its not good to give up'),
+    new Quote(2,'Remain calm it does not hurt','always be calm'),
+    new Quote(3, 'Never chase a Dream','remain focused'),
+    new Quote(4, 'when the deal is too good think twice','think before doing anything'),
+    new Quote(5, 'character makes a man','be a person of character'),
+    new Quote(6, 'work hard today to avoid hardwork tommorrow','always be hardworking'),
 
-  constructor() { }
+  ]
 
-  ngOnInit() {
+  toogleDetails(index){
+      this.quotes[index].showDescription = !this.quotes[index].showDescription;
   }
+
+constructor() { }
+ngOnInit() {
+}
 }
