b0a09a0618c261659aa5647bd6a32b33ee1d5fc8
ngishjonathan <balozimwash@gmil.com>
2019-04-29 10:55:02 +0300

    created ngSubmit event attribute that calls the submitQuote

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit,Output,EventEmitter } from '@angular/core';
+import {Quote} from '../quote'
 @Component({
   selector: 'app-quote-form',
   templateUrl: './quote-form.component.html',
   styleUrls: ['./quote-form.component.css']
 })
 export class QuoteFormComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  newQuote=new Quote(0,"","",)
+  @Output() addQuote=new EventEmitter<Quote>();
+  
+  submitGoal(){
+      this.addQuote.emit(this.newQuote);
+      }
+      ngOnInit() {
   }
-
 }
+
